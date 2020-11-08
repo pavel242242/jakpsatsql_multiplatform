b03ea8dec5e7ca93e3ea5fb708e705f6186a59c0
pavel242242 <pavel@chocholous.net>
2020-11-08 21:31:01 +0000

    config gitpod

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todos</h1>
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{todo.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  data() {
+    return {
+      todos: []
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get(`https://8080-ae824467-344c-46b2-8adf-c18a6d5f27c9.ws-eu01.gitpod.io/todos`)
+
+      this.todos = res.data;
+    } catch(e) {
+      console.error(e)
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #6c5ce7;
   margin-top: 60px;
+}
+
+li {
+  list-style: none;
+}
+
+h1 {
+  color: #a29bfe
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js
+module.exports = {
+    // options...
+    devServer: {
+        host: '0.0.0.0',
+        allowedHosts: ['localhost', '.gitpod.io'],
+    }
+}
+
