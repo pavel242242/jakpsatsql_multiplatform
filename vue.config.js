6c40e1e1e069e3dd069358ed8708e09a79742c85
pavel242242 <pavel@chocholous.net>
2020-11-08 22:03:34 +0000

    allowedhosts

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // options...
     devServer: {
         host: '0.0.0.0',
-        allowedHosts: ['localhost', 'e6736bea-7f19-41cb-a107-609fb616b118.ws-eu01.gitpod.io'],
+        allowedHosts: ['localhost', '.gitpod.io'],
     }
 }
 
